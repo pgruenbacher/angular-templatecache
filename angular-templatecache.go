bfd602695b7e68437f9c1541f67a02228d7eb61d
omeid <public@omeid.me>
2015-02-24 07:49:12 +1100

    fix log output.

--- angular-templatecache.go
+++ angular-templatecache.go
@@ -40,7 +40,7 @@ func Build(c *slurp.C, config Config) slurp.Stage {
 		for file := range in {
 			path, _ := filepath.Rel(file.Dir, file.Path)
 			path = filepath.ToSlash(path)
-			c.Infof("Adding %s.\n", path)
+			c.Infof("Adding %s", path)
 			b.Files[path] = file
 			defer file.Close() //Close files AFTER we have build our package.
 		}
